6bedec786390f36f29bef8efd05115566cd70984
Matheus Zauza Maschietto <matheus_maschietto@yahoo.com.br>
2023-08-08 19:44:56 -0300

    update

--- Atividade1/src/Classes/Pessoa.java
+++ Atividade1/src/Classes/Pessoa.java
@@ -1,2 +1,29 @@
-package Classes;public class Pessoa {
+package Classes;
+
+import java.util.Date;
+
+public class Pessoa {
+    private String nome;
+    private Date dataNascimento;
+    private int idade;
+
+    public String getNome(){
+        return this.nome;
+    }
+    public Date getDataNascimento(){
+        return this.dataNascimento;
+    }
+    public int getIdade(){
+        return this.idade;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setDataNascimento(Date dataNascimento){
+        this.dataNascimento = dataNascimento;
+    }
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
 }
---
+++ Atividade1/src/Main.java
@@ -0,0 +1,7 @@
+import Classes.Pessoa;
+
+public class Main {
+    public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa();
+    }
+}
